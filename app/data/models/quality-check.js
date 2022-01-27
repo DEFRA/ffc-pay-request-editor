5a86c4fe22bd592838d09529c60c4e98843442c4
Paul Popa <paulp@kainos.com>
2022-01-27 09:39:58 +0000

    SFI-1568: Request editor needs database setup

---
+++ app/data/models/quality-check.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, DataTypes) => {
+  const qualityCheck = sequelize.define('qualityCheck', {
+    qualityCheckId: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    paymentRequestId: DataTypes.INTEGER,
+    checkedDate: DataTypes.DATE,
+    checkedBy: DataTypes.STRING,
+    status: DataTypes.STRING
+  },
+  {
+    tableName: 'qualityChecks',
+    freezeTableName: true,
+    timestamps: false
+  })
+  qualityCheck.associate = function (models) {
+    qualityCheck.belongsTo(models.paymentRequest, {
+      foreignKey: 'paymentRequestId',
+      as: 'paymentRequest'
+    })
+  }
+  return qualityCheck
+}
---
+++ app/data/models/scheme.js
@@ -0,0 +1,22 @@
+module.exports = (sequelize, DataTypes) => {
+  const scheme = sequelize.define('scheme', {
+    schemeId: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    name: DataTypes.STRING
+  },
+  {
+    tableName: 'schemes',
+    freezeTableName: true,
+    timestamps: false
+  })
+  scheme.associate = function (models) {
+    scheme.hasMany(models.paymentRequest, {
+      foreignKey: 'schemeId',
+      as: 'paymentRequests'
+    })
+    scheme.hasMany(models.debtData, {
+      foreignKey: 'schemeId',
+      as: 'debtData'
+    })
+  }
+  return scheme
+}
