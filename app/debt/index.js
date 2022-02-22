3193d04995a7f21fe6011043cf1cd93121857148
Simon Dunn <simondunn90@gmail.com>
2022-02-22 14:47:46 +0000

    fixed timeout error by removing lock:true

--- app/debt/index.js
+++ app/debt/index.js
@@ -2,7 +2,7 @@ const getDebts = require('./get-debts')
 const getDebtsCount = require('./get-debts-count')
 const checkDebts = require('./check-debts')
 const attachDebtInformation = require('./attach-debt-information')
-const getQualityCheck = require('./get-quality-check')
+const getQualityCheck = require('../quality-check/get-quality-check')
 
 module.exports = {
   getDebts,
