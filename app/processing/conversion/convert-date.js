3d42e6a4c3d122cb9c9846db445b9db2cdfc7155
John Watson <johnwatson484@hotmail.com>
2022-02-23 19:45:57 +0000

    Update date format

--- app/processing/conversion/convert-date.js
+++ app/processing/conversion/convert-date.js
@@ -3,7 +3,7 @@ function convertDateToDDMMYYYY (dd, mm, yyyy) {
   const month = String(mm).padStart(2, '0')
   const year = String(yyyy)
 
-  return `${day}-${month}-${year}`
+  return `${day}/${month}/${year}`
 }
 
 module.exports = { convertDateToDDMMYYYY }
