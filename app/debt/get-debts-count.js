71a1f0eccb283d9116cdb2c335f27ce8b4ffb736
John Watson <johnwatson484@hotmail.com>
2022-02-23 17:04:35 +0000

    Bug fixes

--- app/debt/get-debts-count.js
+++ app/debt/get-debts-count.js
@@ -1,7 +1,11 @@
 const db = require('../data')
 
 const getDebtsCount = async () => {
-  return db.debtData.count()
+  return db.debtData.count({
+    where: {
+      paymentRequestId: null
+    }
+  })
 }
 
 module.exports = getDebtsCount
