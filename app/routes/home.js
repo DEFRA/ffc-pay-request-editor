2e46893fc216638e08406877a7cbb84f3e9107b1
Marc Templeton <marcte@kainos.com>
2022-02-24 10:57:39 +0000

    successfully pushing messages to qc topic

--- app/routes/home.js
+++ app/routes/home.js
@@ -2,11 +2,16 @@ const { getDebtsCount } = require('../debt')
 const { getPaymentRequestCount } = require('../payment-request')
 const { getQualityChecksCount } = require('../quality-check')
 
+const sendCompletedRequests = require('../processing/quality-check')
+
 module.exports = {
   method: 'GET',
   path: '/',
   options: {
     handler: async (request, h) => {
+      console.log('sending message req')
+      await sendCompletedRequests()
+
       return h.view('home',
         {
           captureCount: await getDebtsCount(),
