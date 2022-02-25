71866871fe22399e89bdf295aaad1f2826bd3efa
John Watson <42574977+johnwatson484@users.noreply.github.com>
2022-02-25 15:25:13 +0000

    Minor bug fixes (#14)

--- app/inbound/quality-checks.js
+++ app/inbound/quality-checks.js
@@ -1,7 +1,7 @@
 const db = require('../data')
 
 const updateQualityCheck = async (paymentRequestId) => {
-  await db.qualityCheck.create({ paymentRequestId, status: 'Pending' })
+  await db.qualityCheck.create({ paymentRequestId, status: 'Not ready' })
 }
 
 module.exports = updateQualityCheck
