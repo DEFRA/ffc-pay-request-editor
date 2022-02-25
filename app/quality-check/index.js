bb3cf388b689c74c514fad23494c436944cd32e3
Simon Dunn <simondunn90@gmail.com>
2022-02-25 13:47:16 +0000

    remove quality-check code

--- app/quality-check/index.js
+++ app/quality-check/index.js
@@ -1,9 +1,7 @@
 const getQualityChecks = require('./get-quality-checks')
 const getQualityChecksCount = require('./get-quality-checks-count')
-const getSingleQualityCheck = require('./get-single-quality-check')
 
 module.exports = {
   getQualityChecks,
-  getQualityChecksCount,
-  getSingleQualityCheck
+  getQualityChecksCount
 }
