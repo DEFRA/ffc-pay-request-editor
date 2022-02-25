dae66e5288d5ef2f15d1592230d119dc816fbe2a
Marc Templeton <marcte@kainos.com>
2022-02-25 10:17:40 +0000

    convertor unit more descriptive names, split into different describes and edge case of empty string and 0 added

--- test/unit/processing/convert-concurrency.test.js
+++ test/unit/processing/convert-concurrency.test.js
@@ -3,29 +3,41 @@ const {
   convertToPounds
 } = require('../../../app/processing/conversion')
 
-describe('convert currency', () => {
-  test('converts 100 to pence', () => {
+describe('Pence convertor', () => {
+  test('should convert 100 to pence', () => {
     const result = convertStringToPence('100')
     expect(result).toEqual(10000)
   })
 
-  test('converts 100.10 to pence', () => {
+  test('should convert 100.10 to pence', () => {
     const result = convertStringToPence('100.10')
     expect(result).toEqual(10010)
   })
 
-  test('converts 100.1 to pence', () => {
+  test('should convert 100.1 to pence', () => {
     const result = convertStringToPence('100.1')
     expect(result).toEqual(10010)
   })
 
-  test('converts 10000 to pounds', () => {
+  test('should convert empty string to 0 pence', () => {
+    const result = convertStringToPence('')
+    expect(result).toEqual(0)
+  })
+})
+
+describe('Pound convertor', () => {
+  test('should convert 10000 number to pounds', () => {
     const result = convertToPounds(10000)
     expect(result).toEqual(100.00)
   })
 
-  test('converts 10010 to pounds', () => {
+  test('should convert 10010 number to pounds', () => {
     const result = convertToPounds(10010)
     expect(result).toEqual(100.10)
+  })
+
+  test('should convert 0 number to pounds', () => {
+    const result = convertToPounds(0)
+    expect(result).toEqual(0.00)
   })
 })
