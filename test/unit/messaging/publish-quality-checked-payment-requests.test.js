6774566931e35a49f69f485bbaad82bc74ec3894
Marc Templeton <marcte@kainos.com>
2022-03-15 10:26:34 +0000

    increasing test coverage

--- test/unit/messaging/publish-quality-checked-payment-requests.test.js
+++ test/unit/messaging/publish-quality-checked-payment-requests.test.js
@@ -1,13 +1,32 @@
 const { SCHEME_ID_SFI_PILOT } = require('../../data/scheme-id')
 
-const { publishPaymentRequest } = require('../../../app/messaging/publish-quality-checked-payment-request')
+jest.mock('../../../app/payment-request')
+const {
+  getQualityCheckedPaymentRequests,
+  updatePaymentRequestReleased
+} = require('../../../app/payment-request')
+
+const {
+  publishQualityCheckedPaymentRequests,
+  publishPaymentRequest
+} = require('../../../app/messaging/publish-quality-checked-payment-request')
 
 describe('Publish quality checked payment requests', () => {
+  let qualityCheckedPaymentRequests
   let qualityCheckSender
   let paymentRequest
   let message
 
   beforeEach(async () => {
+    qualityCheckedPaymentRequests = [{
+      paymentRequestId: 1,
+      invoiceNumber: 'SFI123',
+      frn: 1234567890,
+      debtType: undefined,
+      recoveryDate: undefined
+    }]
+    getQualityCheckedPaymentRequests.mockReturnValue(JSON.parse(JSON.stringify(qualityCheckedPaymentRequests)))
+
     qualityCheckSender = { sendMessage: jest.fn() }
 
     paymentRequest = {
@@ -26,6 +45,21 @@ describe('Publish quality checked payment requests', () => {
 
   afterEach(() => {
     jest.clearAllMocks()
+  })
+
+  test('calls internal loop function', async () => {
+    await publishQualityCheckedPaymentRequests(qualityCheckSender)
+    expect(updatePaymentRequestReleased).toHaveBeenCalledTimes(1)
+    expect(updatePaymentRequestReleased).toHaveBeenCalledWith(qualityCheckedPaymentRequests[0].paymentRequestId)
+  })
+
+  test('error is caught and returned', async () => {
+    getQualityCheckedPaymentRequests.mockImplementation(() => {
+      throw new Error()
+    })
+
+    await publishQualityCheckedPaymentRequests(qualityCheckSender)
+    expect(updatePaymentRequestReleased).not.toHaveBeenCalled()
   })
 
   test('completes valid message', async () => {
