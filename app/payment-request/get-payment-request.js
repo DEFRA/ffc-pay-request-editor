032338ce3f87b7acb6847e41a55d0515b0c48ddd
John Watson <johnwatson484@hotmail.com>
2022-03-16 21:16:59 +0000

    Filter

--- app/payment-request/get-payment-request.js
+++ app/payment-request/get-payment-request.js
@@ -1,27 +1,30 @@
 const db = require('../data')
 
 const getPaymentRequest = async () => {
-  return db.paymentRequest.findAll(
-    {
-      include: [
-        {
-          model: db.scheme,
-          as: 'schemes',
-          attributes: ['name']
-        }
-      ],
-      attributes: [
-        'paymentRequestId',
-        'frn',
-        'agreementNumber',
-        'invoiceNumber',
-        'paymentRequestNumber',
-        'value',
-        'received',
-        'receivedFormatted',
-        'ledger'
-      ]
-    })
+  return db.paymentRequest.findAll({
+    include: [{
+      model: db.scheme,
+      as: 'schemes',
+      attributes: ['name']
+    }, {
+      model: db.debtData,
+      as: 'debtData'
+    }],
+    where: {
+      $debtData$: null
+    },
+    attributes: [
+      'paymentRequestId',
+      'frn',
+      'agreementNumber',
+      'invoiceNumber',
+      'paymentRequestNumber',
+      'value',
+      'received',
+      'receivedFormatted',
+      'ledger'
+    ]
+  })
 }
 
 const getPaymentRequestByInvoiceNumber = async (invoiceNumber) => {
