6e4a0c260e5e23cfc4600c3a1853140e62e82b5f
John Watson <johnwatson484@hotmail.com>
2022-03-17 19:43:10 +0000

    Add delete

--- app/routes/capture.js
+++ app/routes/capture.js
@@ -1,6 +1,8 @@
 const ViewModel = require('./models/search')
 const { getDebts } = require('../debt')
 const schema = require('./schemas/capture')
+const Joi = require('joi')
+const deleteDebt = require('../debt/delete-debt')
 const searchLabelText = 'Search for data by FRN number'
 
 module.exports = [{
@@ -36,5 +38,22 @@ module.exports = [{
       return h.view('capture', new ViewModel(searchLabelText, frn, { message: 'No debts match the FRN provided.' })).code(400)
     }
   }
-}
-]
+}, {
+  method: 'POST',
+  path: '/capture/delete',
+  options: {
+    validate: {
+      payload: Joi.object({
+        debtDataId: Joi.number().integer().required()
+      }),
+      failAction: async (request, h, error) => {
+        const captureData = await getDebts()
+        return h.view('capture', { captureData, ...new ViewModel(searchLabelText, request.payload.frn, error) }).code(400).takeover()
+      }
+    },
+    handler: async (request, h) => {
+      await deleteDebt(request.payload.debtDataId)
+      return h.redirect('/capture')
+    }
+  }
+}]
