1fb8c23f3e1f4beb49d3b2a7a9df88c576a98f45
Marc Templeton <56440327+marctemp@users.noreply.github.com>
2022-03-18 15:22:48 +0000

    Display full named debt types in capture screen (#24)
    * virtual dat type used to convert dent type from debt data table to fullnamed

--- app/debt-types.js
+++ app/debt-types.js
@@ -1,4 +1,17 @@
+const DEBT_TYPES = [{ id: 'adm', text: 'Administrative' }, { id: 'irr', text: 'Irregular' }]
+
+const DEBT_IDS = DEBT_TYPES.map(debt => debt.id)
+
+const ADMINISTRATIVE = DEBT_TYPES.find(debt => debt.id === 'adm').id
+const ADMINISTRATIVE_TEXT = DEBT_TYPES.find(debt => debt.id === 'adm').text
+
+const IRREGULAR = DEBT_TYPES.find(debt => debt.id === 'irr').id
+const IRREGULAR_TEXT = DEBT_TYPES.find(debt => debt.id === 'irr').text
+
 module.exports = {
-  ADMINISTRATIVE: 'adm',
-  IRREGULAR: 'irr'
+  DEBT_IDS,
+  ADMINISTRATIVE,
+  ADMINISTRATIVE_TEXT,
+  IRREGULAR,
+  IRREGULAR_TEXT
 }
