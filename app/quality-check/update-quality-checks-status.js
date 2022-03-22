aa5c01e4d951507cd77aece897a90a6ab5e04b51
steve-dickinson <steve.dickinson@defra.gov.uk>
2022-03-22 06:50:40 +0000

    update query

--- app/quality-check/update-quality-checks-status.js
+++ app/quality-check/update-quality-checks-status.js
@@ -1,7 +1,7 @@
 const db = require('../data')
 
 const updateQualityChecksStatus = async (paymentRequestId, newStatus, transaction) => {
-  return db.qualityCheck.update({ status: newStatus, checkedDate: new Date() }, { where: { paymentRequestId: paymentRequestId } }, { transaction })
+  return db.qualityCheck.update({ status: newStatus, checkedDate: new Date() }, { where: { paymentRequestId } }, { transaction })
 }
 
 module.exports = updateQualityChecksStatus
