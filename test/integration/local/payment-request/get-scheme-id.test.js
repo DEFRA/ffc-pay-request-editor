7fd3a5caa352c7fb4cac4af021c809a3b17bcea7
Simon Dunn <simondunn90@gmail.com>
2022-03-23 16:02:34 +0000

    update tests

---
+++ test/integration/local/payment-request/get-scheme-id.test.js
@@ -0,0 +1,34 @@
+const db = require('../../../../app/data')
+const { getSchemeId } = require('../../../../app/processing/scheme')
+
+describe('Get scheme id test', () => {
+  let scheme
+  const resetData = async () => {
+    await db.scheme.truncate({ cascade: true })
+  }
+  beforeEach(async () => {
+    await resetData()
+
+    scheme = {
+      schemeId: 1,
+      name: 'somename'
+    }
+
+    await db.scheme.create(scheme)
+  })
+
+  afterAll(async () => {
+    await resetData()
+    await db.sequelize.close()
+  })
+
+  test('Return schemeId where name matches', async () => {
+    const foundSchemeId = await getSchemeId(scheme.name)
+    expect(foundSchemeId).toBe(1)
+  })
+
+  test('Return undefined where invalid name given', async () => {
+    const foundSchemeId = await getSchemeId('123')
+    expect(foundSchemeId).toBe(undefined)
+  })
+})
