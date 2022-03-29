cb2fc26c34f25d175a91583d753955760ae658ef
Simon Dunn <simondunn90@gmail.com>
2022-03-29 15:37:50 +0100

    setup send event config

--- app/messaging/process-manual-ledger-data-message.js
+++ app/messaging/process-manual-ledger-data-message.js
@@ -1,4 +1,5 @@
 const { processManualLedgerRequest } = require('../manual-ledger')
+const raiseEvent = require('../event')
 const util = require('util')
 
 const processManualLedgerDataMessage = async (message, receiver) => {
@@ -7,6 +8,7 @@ const processManualLedgerDataMessage = async (message, receiver) => {
     console.log('Payment request received for manual ledger check', util.inspect(manualLedgerRequest, false, null, true))
     await processManualLedgerRequest(manualLedgerRequest)
     await receiver.completeMessage(message)
+    await raiseEvent({ id: 'ffc-pay-request-editor', name: 'Manual ledger received', type: 'info', message: 'Manual ledger received', data: manualLedgerRequest })
   } catch (err) {
     console.error('Unable to process manual ledger message:', err)
   }
