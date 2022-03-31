d3a99aae0dd454c8a954b6e68ff7a83cc554c1c5
steve-dickinson <steve.dickinson@defra.gov.uk>
2022-03-31 13:58:16 +0100

    Update to events

--- app/event/send-manual-ledger-check-event.js
+++ app/event/send-manual-ledger-check-event.js
@@ -1,14 +1,14 @@
 const raiseEvent = require('./raise-event')
 const getCorrelationId = require('../payment-request/get-correlation-id')
 
-const sendManualLedgerCheckEvent = async (paymentRequestId, provisionalLedgerData) => {
+const sendManualLedgerCheckEvent = async (paymentRequestId, user, provisionalLedgerData) => {
   const { correlationId } = await getCorrelationId(paymentRequestId)
   const event = {
     id: correlationId,
     name: 'payment-request-manual-ledger-check-event',
     type: 'info',
     message: 'Manual ledger check confirmed ready to be reviewed.',
-    data: { provisionalLedgerData }
+    data: { provisionalLedgerData, user }
   }
   await raiseEvent(event)
 }
--- app/event/send-manual-ledger-review-event.js
+++ app/event/send-manual-ledger-review-event.js
@@ -1,14 +1,14 @@
 const raiseEvent = require('./raise-event')
 const getCorrelationId = require('../payment-request/get-correlation-id')
 
-const sendManualLedgerReviewEvent = async (paymentRequestId, status) => {
+const sendManualLedgerReviewEvent = async (paymentRequestId, user, status) => {
   const { correlationId } = await getCorrelationId(paymentRequestId)
   const event = {
     id: correlationId,
     name: 'payment-request-quality-review-event',
     type: 'info',
     message: 'Manual ledger check sucessfully reviewed.',
-    data: { status }
+    data: { status, user }
   }
   await raiseEvent(event)
 }
