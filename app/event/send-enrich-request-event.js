c161577affa3c79da80043174b85b5722123d1b0
steve-dickinson <steve.dickinson@defra.gov.uk>
2022-03-31 14:00:32 +0100

    \update event to include user

--- app/event/send-enrich-request-event.js
+++ app/event/send-enrich-request-event.js
@@ -1,7 +1,7 @@
 const raiseEvent = require('./raise-event')
 const getCorrelationId = require('../payment-request/get-correlation-id')
 
-const sendEnrichRequestEvent = async (paymentRequest) => {
+const sendEnrichRequestEvent = async (paymentRequest, user) => {
   const { paymentRequestId } = paymentRequest
   const { correlationId } = await getCorrelationId(paymentRequestId)
   const event = {
@@ -9,7 +9,7 @@ const sendEnrichRequestEvent = async (paymentRequest) => {
     name: 'payment-request-enrich-request-event',
     type: 'info',
     message: 'Debt data succesfully enriched.',
-    data: { paymentRequest }
+    data: { paymentRequest, user }
   }
   await raiseEvent(event)
 }
