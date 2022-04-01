85949e8f0c110a1ed32e97afa6bd22e0c04a2766
Abidemi Adio <abidemi.adio@defra.gov.uk>
2022-04-01 13:54:42 +0100

    Resolved code smell

--- app/manual-ledger/check-manual-ledger-awaiting-debt-data.js
+++ app/manual-ledger/check-manual-ledger-awaiting-debt-data.js
@@ -2,12 +2,13 @@ const db = require('../data')
 const { AWAITING_ENRICHMENT } = require('../quality-check/statuses')
 
 const isManualLedgerAwaitingDebtData = async (paymentRequestId) => {
-  return await db.qualityCheck.findOne({
+  const inManualLedgerAwaitingDebtData = await db.qualityCheck.findOne({
     where: {
       paymentRequestId,
       status: AWAITING_ENRICHMENT
     }
   })
+  return inManualLedgerAwaitingDebtData
 }
 
 module.exports = isManualLedgerAwaitingDebtData
