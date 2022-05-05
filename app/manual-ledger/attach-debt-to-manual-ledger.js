8d326ccd06e3c9cf2d70c8935d7678761abd5f16
steve-dickinson <steve.dickinson@defra.gov.uk>
2022-05-05 10:26:30 +0100

    Attach debt information it AR

--- app/manual-ledger/attach-debt-to-manual-ledger.js
+++ app/manual-ledger/attach-debt-to-manual-ledger.js
@@ -1,18 +1,34 @@
 const db = require('../data')
 
-const attachDebtToManualLedger = async (paymentRequest) => {
+const attachDebtToManualLedger = async (qualityCheckedPaymentRequest, addToAR = false) => {
+  const paymentRequest = qualityCheckedPaymentRequest.paymentRequest
   const debtData = await db.debtData.findOne({
     where: {
       paymentRequestId: paymentRequest.paymentRequestId
     }
   })
 
+  addDebtData(debtData, paymentRequest)
+
+  if (addToAR) {
+    attachDebtToAR(debtData, qualityCheckedPaymentRequest.paymentRequests)
+  }
+
+  return qualityCheckedPaymentRequest
+}
+
+const attachDebtToAR = (debtData, paymentRequests) => {
+  const arLedger = paymentRequests.find(x => x.ledger === 'AR')
+  arLedger.debtType = debtData.debtType
+  arLedger.recoveryDate = debtData.recoveryDate
+}
+
+const addDebtData = (debtData, paymentRequest) => {
   if (debtData) {
     paymentRequest.debtType = debtData.debtType
     paymentRequest.recoveryDate = debtData.recoveryDate
     paymentRequest.attachedDate = debtData.attachedDate
   }
-  return paymentRequest
 }
 
 module.exports = attachDebtToManualLedger
