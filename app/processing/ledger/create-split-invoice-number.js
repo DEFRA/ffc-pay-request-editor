4e2014871e2cdf51dc0fb8ad7d61d410162f898d
John Watson <johnwatson484@hotmail.com>
2022-06-14 11:59:43 +0100

    Update variables

--- app/processing/ledger/create-split-invoice-number.js
+++ app/processing/ledger/create-split-invoice-number.js
@@ -10,11 +10,21 @@ const createSplitInvoiceNumber = (invoiceNumber, splitId, schemeId) => {
 }
 
 const createSitiAgriInvoiceNumber = (invoiceNumber, splitId) => {
-  return `${invoiceNumber.slice(0, 8)}${splitId}${invoiceNumber.slice(8, invoiceNumber.length - 3)}${invoiceNumber.slice(invoiceNumber.length - 2)}`
+  // an invoice number is a string made up of three elements, to inject a split id character into the string we need to identify the position of those elements
+  // we also need to trim the first character of the final element
+  const firstElementLength = 8
+  const originalFinalElementLength = 3
+  const newFinalElementLength = 2
+  return `${invoiceNumber.slice(0, firstElementLength)}${splitId}${invoiceNumber.slice(firstElementLength, invoiceNumber.length - originalFinalElementLength)}${invoiceNumber.slice(invoiceNumber.length - newFinalElementLength)}`
 }
 
 const createDefaultInvoiceNumber = (invoiceNumber, splitId) => {
-  return `${invoiceNumber.slice(0, invoiceNumber.length - 4)}${splitId}${invoiceNumber.slice(invoiceNumber.length - 4, invoiceNumber.length - 3)}${invoiceNumber.slice(invoiceNumber.length - 2)}`
+  // an invoice number is a string made up of two elements, to inject a split id character into the string we need to identify the position of those elements
+  // we also need to trim the first character of the final element
+  const finalElementIndexLength = 4
+  const originalFinalElementLength = 3
+  const newFinalElementLength = 2
+  return `${invoiceNumber.slice(0, invoiceNumber.length - finalElementIndexLength)}${splitId}${invoiceNumber.slice(invoiceNumber.length - finalElementIndexLength, invoiceNumber.length - originalFinalElementLength)}${invoiceNumber.slice(invoiceNumber.length - newFinalElementLength)}`
 }
 
 module.exports = createSplitInvoiceNumber
