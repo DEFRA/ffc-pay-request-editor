6d12ad97571f25e54234c8e3bd641b35d2258a67
John Watson <42574977+johnwatson484@users.noreply.github.com>
2022-06-23 16:47:39 +0100

    Support Lump Sums invoice (#53)

--- app/routes/schemas/application-identifier.js
+++ app/routes/schemas/application-identifier.js
@@ -1,29 +1,5 @@
 const Joi = require('joi')
 
-const LENGTH = 15
-
 module.exports = {
-  applicationIdentifier: Joi.string().regex(/^\w{15}$/).required()
-    .error(errors => {
-      errors.forEach(err => {
-        switch (err.code) {
-          case 'string.empty':
-            err.message = 'The agreement number cannot be empty.'
-            break
-          case 'string.pattern.base':
-            if (err.local.value.length < LENGTH) {
-              err.message = `The agreement number is too short. This must be ${LENGTH} characters.`
-            } else if (err.local.value.length > LENGTH) {
-              err.message = `The agreement number is too long. This must be ${LENGTH} characters.`
-            } else {
-              err.message = 'The agreement number can only have alphanumeric characters.'
-            }
-            break
-          default:
-            err.message = 'The agreement number is invalid.'
-            break
-        }
-      })
-      return errors
-    })
+  applicationIdentifier: Joi.string().regex(/^\w*$/).required().messages({ '*': 'The agreement number is invalid' })
 }
