073274fd87951b85c2b15712c847184799a82560
John Watson <johnwatson484@hotmail.com>
2022-10-06 09:42:33 +0100

    Add tests

--- test/integration/narrow/routes/manual-ledger-check.test.js
+++ test/integration/narrow/routes/manual-ledger-check.test.js
@@ -4,8 +4,13 @@ describe('Manual-ledger-check tests', () => {
   jest.mock('ffc-messaging')
   jest.mock('../../../../app/plugins/crumb')
   jest.mock('../../../../app/manual-ledger')
+  jest.mock('../../../../app/event')
   jest.mock('../../../../app/auth')
   const mockAuth = require('../../../../app/auth')
+  jest.mock('../../../../app/auth/get-user')
+  const mockGetUser = require('../../../../app/auth/get-user')
+  jest.mock('../../../../app/session-handler')
+  const mockSessionHandler = require('../../../../app/session-handler')
   const { getManualLedgers, getManualLedger, calculateManualLedger } = require('../../../../app/manual-ledger')
   const createServer = require('../../../../app/server')
 
@@ -16,6 +21,9 @@ describe('Manual-ledger-check tests', () => {
     username: 'Developer'
   }
 
+  const method = 'GET'
+  const manualLedgerCheckUrl = '/manual-ledger-check'
+
   let server
   let paymentRequest
 
@@ -24,6 +32,9 @@ describe('Manual-ledger-check tests', () => {
     await server.initialize()
 
     mockAuth.getUser.mockResolvedValue(user)
+    mockGetUser.mockResolvedValue(user)
+
+    mockSessionHandler.get.mockReturnValue({ provisionalLedgerData: {} })
 
     getManualLedgers.mockResolvedValue([{
       paymentRequest: {
@@ -44,9 +55,6 @@ describe('Manual-ledger-check tests', () => {
   })
 
   describe('GET /manual-ledger-check requests', () => {
-    const method = 'GET'
-    const manualLedgerCheckUrl = '/manual-ledger-check'
-
     test('GET /manual-ledger-check returns manual-ledger-check view', async () => {
       getManualLedger.mockResolvedValue(paymentRequest)
       const options = {
@@ -155,6 +163,36 @@ describe('Manual-ledger-check tests', () => {
       const response = await server.inject(options)
       expect(response.statusCode).toBe(500)
       expect(response.request.response.source.template).toBe('500')
+    })
+
+    test('POST /manual-ledger-check with valid payload redirects to /quality-check if agree', async () => {
+      const options = {
+        method: 'POST',
+        auth,
+        url: manualLedgerCheckUrl,
+        payload: {
+          paymentRequestId: '1',
+          agree: true
+        }
+      }
+      const response = await server.inject(options)
+      expect(response.statusCode).toBe(302)
+      expect(response.headers.location).toBe('/quality-check')
+    })
+
+    test('POST /manual-ledger-check with valid payload returns manual-ledger-check view if disagree', async () => {
+      const options = {
+        method: 'POST',
+        auth,
+        url: manualLedgerCheckUrl,
+        payload: {
+          paymentRequestId: '1',
+          agree: false
+        }
+      }
+      const response = await server.inject(options)
+      expect(response.statusCode).toBe(200)
+      expect(response.request.response.source.template).toBe('manual-ledger-check')
     })
   })
 })
