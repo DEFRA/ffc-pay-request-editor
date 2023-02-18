8bd16b9a13daf9e5e01a1c26b8d65675967b64d4
John Watson <johnwatson484@hotmail.com>
2023-02-18 12:14:21 +0000

    Add events

--- app/event/send-manual-ledger-check-event.js
+++ app/event/send-manual-ledger-check-event.js
@@ -1,7 +1,19 @@
 const raiseEvent = require('./raise-event')
 const getCorrelationId = require('../payment-request/get-correlation-id')
+const config = require('../config')
+const { EventPublisher } = require('ffc-pay-event-publisher')
+const { getPaymentRequestByRequestId } = require('../payment-request')
 
 const sendManualLedgerCheckEvent = async (paymentRequestId, user, provisionalLedgerData) => {
+  if (config.useV1Events) {
+    await sendV1ManualLedgerCheckEvent(paymentRequestId, user, provisionalLedgerData)
+  }
+  if (config.useV2Events) {
+    await sendV2ManualLedgerCheckEvent(paymentRequestId, user, provisionalLedgerData)
+  }
+}
+
+const sendV1ManualLedgerCheckEvent = async (paymentRequestId, user, provisionalLedgerData) => {
   const correlationId = await getCorrelationId(paymentRequestId)
   const event = {
     id: correlationId,
@@ -11,6 +23,21 @@ const sendManualLedgerCheckEvent = async (paymentRequestId, user, provisionalLed
     data: { provisionalLedgerData, user }
   }
   await raiseEvent(event)
+}
+
+const sendV2ManualLedgerCheckEvent = async (paymentRequestId, user, provisionalLedgerData) => {
+  const paymentRequest = await getPaymentRequestByRequestId(paymentRequestId)
+  const event = {
+    source: 'ffc-pay-request-editor',
+    type: 'uk.gov.defra.ffc.pay.payment.ledger.assigned',
+    data: {
+      assignedBy: user,
+      ...provisionalLedgerData,
+      ...paymentRequest
+    }
+  }
+  const eventPublisher = new EventPublisher(config.eventsTopic)
+  await eventPublisher.publishEvent(event)
 }
 
 module.exports = sendManualLedgerCheckEvent
--- app/event/send-manual-ledger-review-event.js
+++ app/event/send-manual-ledger-review-event.js
@@ -1,7 +1,19 @@
 const raiseEvent = require('./raise-event')
 const getCorrelationId = require('../payment-request/get-correlation-id')
+const config = require('../config')
+const { getPaymentRequestByRequestId } = require('../payment-request')
+const { EventPublisher } = require('ffc-pay-event-publisher')
 
 const sendManualLedgerReviewEvent = async (paymentRequestId, user, status) => {
+  if (config.useV1Events) {
+    await sendV1ManualLedgerReviewEvent(paymentRequestId, user, status)
+  }
+  if (config.useV2Events) {
+    await sendV2ManualLedgerReviewEvent(paymentRequestId, user, status)
+  }
+}
+
+const sendV1ManualLedgerReviewEvent = async (paymentRequestId, user, status) => {
   const correlationId = await getCorrelationId(paymentRequestId)
   const event = {
     id: correlationId,
@@ -11,6 +23,21 @@ const sendManualLedgerReviewEvent = async (paymentRequestId, user, status) => {
     data: { status, user }
   }
   await raiseEvent(event)
+}
+
+const sendV2ManualLedgerReviewEvent = async (paymentRequestId, user, status) => {
+  const paymentRequest = await getPaymentRequestByRequestId(paymentRequestId)
+  const event = {
+    source: 'ffc-pay-request-editor',
+    type: `uk.gov.defra.ffc.pay.payment.ledger.quality-check.${status}`,
+    data: {
+      qualityCheckedBy: user,
+      status,
+      ...paymentRequest
+    }
+  }
+  const eventPublisher = new EventPublisher(config.eventsTopic)
+  await eventPublisher.publishEvent(event)
 }
 
 module.exports = sendManualLedgerReviewEvent
