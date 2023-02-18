deec7e4ca4522b38f13cdd5692fb4c2799aaa670
John Watson <johnwatson484@hotmail.com>
2023-02-18 12:06:31 +0000

    Update event

--- app/event/send-enrich-request-blocked-event.js
+++ app/event/send-enrich-request-blocked-event.js
@@ -1,7 +1,18 @@
 const raiseEvent = require('./raise-event')
 const getCorrelationId = require('../payment-request/get-correlation-id')
+const config = require('../config')
+const { EventPublisher } = require('ffc-pay-event-publisher')
 
 const sendEnrichRequestBlockedEvent = async (paymentRequest) => {
+  if (config.useV1Events) {
+    await sendV1EnrichRequestBlockedEvent(paymentRequest)
+  }
+  if (config.useV2Events) {
+    await sendV2EnrichRequestBlockedEvent(paymentRequest)
+  }
+}
+
+const sendV1EnrichRequestBlockedEvent = async (paymentRequest) => {
   const { paymentRequestId } = paymentRequest
   if (paymentRequestId) {
     const correlationId = await getCorrelationId(paymentRequestId) ?? ''
@@ -14,6 +25,19 @@ const sendEnrichRequestBlockedEvent = async (paymentRequest) => {
     }
     await raiseEvent(event)
   }
+}
+
+const sendV2EnrichRequestBlockedEvent = async (paymentRequest) => {
+  const event = {
+    source: 'ffc-pay-request-editor',
+    type: 'uk.gov.defra.ffc.pay.warning.payment.debt.missing',
+    data: {
+      message: 'Payment request does not have debt data to attach',
+      ...paymentRequest
+    }
+  }
+  const eventPublisher = new EventPublisher(config.eventsTopic)
+  await eventPublisher.publishEvent(event)
 }
 
 module.exports = sendEnrichRequestBlockedEvent
--- app/event/send-enrich-request-event.js
+++ app/event/send-enrich-request-event.js
@@ -1,7 +1,18 @@
 const raiseEvent = require('./raise-event')
 const getCorrelationId = require('../payment-request/get-correlation-id')
+const config = require('../config')
+const { EventPublisher } = require('ffc-pay-event-publisher')
 
 const sendEnrichRequestEvent = async (paymentRequest, user) => {
+  if (config.useV1Events) {
+    await sendV1EnrichRequestEvent(paymentRequest, user)
+  }
+  if (config.useV2Events) {
+    await sendV2EnrichRequestEvent(paymentRequest, user)
+  }
+}
+
+const sendV1EnrichRequestEvent = async (paymentRequest, user) => {
   const { paymentRequestId } = paymentRequest
   const correlationId = await getCorrelationId(paymentRequestId)
   const event = {
@@ -12,6 +23,19 @@ const sendEnrichRequestEvent = async (paymentRequest, user) => {
     data: { paymentRequest, user }
   }
   await raiseEvent(event)
+}
+
+const sendV2EnrichRequestEvent = async (paymentRequest, user) => {
+  const event = {
+    source: 'ffc-pay-request-editor',
+    type: 'uk.gov.defra.ffc.pay.payment.debt.attached',
+    data: {
+      attachedBy: user,
+      ...paymentRequest
+    }
+  }
+  const eventPublisher = new EventPublisher(config.eventsTopic)
+  await eventPublisher.publishEvent(event)
 }
 
 module.exports = sendEnrichRequestEvent
