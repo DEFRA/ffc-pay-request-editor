49c41164d922b781424a20f8a3ab20c1be8e7295
John Watson <john.watson2@rpa.gov.uk>
2023-03-22 16:33:53 +0000

    Add split tests

--- test/unit/processing/split-ledger.test.js
+++ test/unit/processing/split-ledger.test.js
@@ -124,7 +124,7 @@ describe('split ledger test', () => {
       value: 1000,
       schemeId: CS,
       agreementNumber: '12345678',
-      invoiceNumber: 'S12345678SFI123456V002',
+      invoiceNumber: 'S12345678A123456V002',
       paymentRequestNumber: 2,
       invoiceLines: [{
         description: 'G00',
@@ -135,5 +135,43 @@ describe('split ledger test', () => {
     expect(splitLedger.length).toBe(2)
     expect(splitLedger.filter(x => x.invoiceNumber.startsWith('S1234567A')).length).toBe(1)
     expect(splitLedger.filter(x => x.invoiceNumber.startsWith('S1234567B')).length).toBe(1)
+  })
+
+  test('should update invoice numbers for BPS', () => {
+    const paymentRequest = {
+      ledger: AP,
+      value: 1000,
+      schemeId: BPS,
+      agreementNumber: '12345678',
+      invoiceNumber: 'S12345678C123456V002',
+      paymentRequestNumber: 2,
+      invoiceLines: [{
+        description: 'G00',
+        value: 1000
+      }]
+    }
+    const splitLedger = splitToLedger(paymentRequest, 800, AR)
+    expect(splitLedger.length).toBe(2)
+    expect(splitLedger.filter(x => x.invoiceNumber.startsWith('S1234567A')).length).toBe(1)
+    expect(splitLedger.filter(x => x.invoiceNumber.startsWith('S1234567B')).length).toBe(1)
+  })
+
+  test('should update invoice numbers for FDMR', () => {
+    const paymentRequest = {
+      ledger: AP,
+      value: 1000,
+      schemeId: CS,
+      agreementNumber: '12345678',
+      invoiceNumber: 'F12345678C123456V002',
+      paymentRequestNumber: 2,
+      invoiceLines: [{
+        description: 'G00',
+        value: 1000
+      }]
+    }
+    const splitLedger = splitToLedger(paymentRequest, 800, AR)
+    expect(splitLedger.length).toBe(2)
+    expect(splitLedger.filter(x => x.invoiceNumber.startsWith('F1234567A')).length).toBe(1)
+    expect(splitLedger.filter(x => x.invoiceNumber.startsWith('F1234567B')).length).toBe(1)
   })
 })
