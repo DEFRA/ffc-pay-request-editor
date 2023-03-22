d70846ce2bd0b078c6cc49ea233adeed137cbd16
John Watson <john.watson2@rpa.gov.uk>
2023-03-22 16:26:59 +0000

    Reorder message

--- app/routes/schemas/scheme.js
+++ app/routes/schemas/scheme.js
@@ -3,7 +3,7 @@ const Joi = require('joi')
 module.exports = {
   scheme: Joi.string().valid('SFI', 'SFI Pilot', 'CS', 'BPS', 'FDMR', 'Vet Visits', 'Lump Sums').required()
     .messages({
-      'any.only': 'The scheme must be one of the following: SFI, SFI Pilot, CS, BPS, FDMR, Vet Visits, Lump Sums.',
+      'any.only': 'The scheme must be one of the following: SFI, SFI Pilot, Lump Sums, Vet Visits, CS, BPS, FDMR.',
       'any.required': 'A scheme must be selected.'
     })
 }
