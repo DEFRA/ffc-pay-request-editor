37ee0920679b3abf4083afbbb7bae56ea89b9511
Sam Plackett <samuel.plackett@eviden.com>
2023-08-21 17:05:01 +0100

    added SFI23 constant for invoice splits

--- app/routes/schemas/scheme.js
+++ app/routes/schemas/scheme.js
@@ -1,9 +1,9 @@
 const Joi = require('joi')
 
 module.exports = {
-  scheme: Joi.string().valid('SFI', 'SFI Pilot', 'CS', 'BPS', 'FDMR', 'Vet Visits', 'Lump Sums').required()
+  scheme: Joi.string().valid('SFI', 'SFI Pilot', 'CS', 'BPS', 'FDMR', 'SFI23', 'Vet Visits', 'Lump Sums').required()
     .messages({
-      'any.only': 'The scheme must be one of the following: SFI, SFI Pilot, Lump Sums, Vet Visits, CS, BPS, FDMR.',
+      'any.only': 'The scheme must be one of the following: SFI, SFI Pilot, Lump Sums, Vet Visits, CS, BPS, FDMR, SFI23.',
       'any.required': 'A scheme must be selected.'
     })
 }
--- test/integration/narrow/routes/capture-debt.test.js
+++ test/integration/narrow/routes/capture-debt.test.js
@@ -132,7 +132,7 @@ describe('capture-debt route', () => {
     expect(result.request.response.source.context.model.schemes).toStrictEqual(SCHEMES.map(scheme => scheme.name))
   })
 
-  test('POST /capture-debt with no scheme selected returns "The scheme must be one of the following: SFI, SFI Pilot, Lump Sums, Vet Visits, CS, BPS, FDMR." error message', async () => {
+  test('POST /capture-debt with no scheme selected returns "The scheme must be one of the following: SFI, SFI Pilot, Lump Sums, Vet Visits, CS, BPS, FDMR, SFI23." error message', async () => {
     const options = {
       method: 'POST',
       url: '/capture-debt',
@@ -141,7 +141,7 @@ describe('capture-debt route', () => {
     }
 
     const result = await server.inject(options)
-    expect(result.request.response.source.context.model.errorSummary[0].text).toEqual('The scheme must be one of the following: SFI, SFI Pilot, Lump Sums, Vet Visits, CS, BPS, FDMR.')
+    expect(result.request.response.source.context.model.errorSummary[0].text).toEqual('The scheme must be one of the following: SFI, SFI Pilot, Lump Sums, Vet Visits, CS, BPS, FDMR, SFI23.')
   })
 
   test('POST /capture-debt with invalid scheme returns 400', async () => {
@@ -181,7 +181,7 @@ describe('capture-debt route', () => {
     expect(result.request.response.source.context.model.schemes).toStrictEqual(SCHEMES.map(scheme => scheme.name))
   })
 
-  test('POST /capture-debt with invalid scheme returns "The scheme must be one of the following: SFI, SFI Pilot, Lump Sums, Vet Visits, CS, BPS, FDMR." error message', async () => {
+  test('POST /capture-debt with invalid scheme returns "The scheme must be one of the following: SFI, SFI Pilot, Lump Sums, Vet Visits, CS, BPS, FDMR, SFI23." error message', async () => {
     const options = {
       method: 'POST',
       url: '/capture-debt',
@@ -190,7 +190,7 @@ describe('capture-debt route', () => {
     }
 
     const result = await server.inject(options)
-    expect(result.request.response.source.context.model.errorSummary[0].text).toEqual('The scheme must be one of the following: SFI, SFI Pilot, Lump Sums, Vet Visits, CS, BPS, FDMR.')
+    expect(result.request.response.source.context.model.errorSummary[0].text).toEqual('The scheme must be one of the following: SFI, SFI Pilot, Lump Sums, Vet Visits, CS, BPS, FDMR, SFI23.')
   })
 
   test('POST /capture-debt with no FRN returns 400', async () => {
