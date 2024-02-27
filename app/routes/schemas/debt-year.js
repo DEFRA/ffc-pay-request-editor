f56b267e07eb7be68779b44bbaeedd680618490e
Sam Plackett <samuel.plackett@eviden.com>
2024-02-27 13:08:21 +0000

    refactor debt date schema

--- app/routes/schemas/debt-year.js
+++ app/routes/schemas/debt-year.js
@@ -1,15 +1,17 @@
 const Joi = require('joi')
 
-const yearNow = new Date().getFullYear()
-
-module.exports = {
-  'debt-discovered-year': Joi.number().integer().min(2015).max(yearNow).required()
-    .messages({
-      'number.max': 'The debt year cannot be in the future.',
-      'number.min': 'The debt year cannot be before 2015.',
-      'number.unsafe': 'The debt year is invalid.',
-      'number.base': 'The debt year must be a number.',
-      'any.required': 'The debt year is required.',
-      '*': 'The debt year must be a number from 2015 to the current year.'
-    })
+const debtYearSchema = (yearNow) => {
+  return {
+    'debt-discovered-year': Joi.number().integer().min(2015).max(yearNow).required()
+      .messages({
+        'number.max': 'The debt year cannot be in the future.',
+        'number.min': 'The debt year cannot be before 2015.',
+        'number.unsafe': 'The debt year is invalid.',
+        'number.base': 'The debt year must be a number.',
+        'any.required': 'The debt year is required.',
+        '*': 'The debt year must be a number from 2015 to the current year.'
+      })
+  }
 }
+
+module.exports = debtYearSchema
