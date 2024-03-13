0e63808d41d560ef630209d584d9d2b7184626c5
Sam Plackett <60177449+samplackett@users.noreply.github.com>
2024-03-13 09:12:38 +0000

    remove comment

--- app/debt/check-debts.js
+++ app/debt/check-debts.js
@@ -6,8 +6,6 @@ const checkDebts = async (schemeId, frn, reference, netValue, transaction) => {
   if (isNaN(parsedFrn)) {
     return {}
   } else {
-    // agreement may have been input without leading A or C char. This is how BPS process in Payment Filter SRE.
-    // need to check the numeric part of the reference for any matches
     const referenceNumeric = reference.match(/\d+/)[0]
 
     return db.debtData.findOne({
