391189ebb4643000bb42bb06dcb20f05d19072c4
Sam Plackett <samuel.plackett@eviden.com>
2024-03-27 10:53:08 +0000

    amend error message for date validation

--- app/routes/schemas/date.js
+++ app/routes/schemas/date.js
@@ -1,13 +1,21 @@
 const Joi = require('joi').extend(require('@joi/date'))
 
 const dateValidation = (inputDate, arrivalDate = 'now') => {
+  let lessErrorMessage = 'Date must not be in the future.'
+  let anyErrorMessage = 'Date must be valid and cannot be in the future.'
+
+  if (arrivalDate !== 'now') {
+    lessErrorMessage = `Date must not be after ${arrivalDate}.`
+    anyErrorMessage = `Date must be valid and cannot be after ${arrivalDate}.`
+  }
+
   return Joi.object({
     date: Joi.date().format('YYYY-MM-DD').less(arrivalDate).required()
       .messages({
         'any.required': 'Date is required.',
         'date.format': 'Date must be in the format YYYY-MM-DD.',
-        'date.less': 'Date must not be in the future.',
-        '*': 'Date must be valid and cannot be in the future.'
+        'date.less': lessErrorMessage,
+        '*': anyErrorMessage
       })
   }).validate(inputDate)
 }
