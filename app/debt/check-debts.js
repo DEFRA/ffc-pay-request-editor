7eadc8aa05327a88d227c1e38ed088a7ac16b917
Sam Plackett <samuel.plackett@eviden.com>
2024-04-25 13:04:58 +0100

    allow debt data matches on both agreement and contract numbers

--- app/debt/check-debts.js
+++ app/debt/check-debts.js
@@ -1,13 +1,13 @@
 const db = require('../data')
 
-const checkDebts = async (schemeId, frn, reference, netValue, transaction) => {
+const checkDebts = async (schemeId, frn, reference, secondaryReference, netValue, transaction) => {
   const parsedFrn = parseInt(frn)
 
   if (isNaN(parsedFrn)) {
     return {}
   } else {
     const referenceNumeric = reference.match(/\d+/)[0]
-
+    const secondaryReferenceNumeric = secondaryReference.match(/\d+/)[0]
     return db.debtData.findOne({
       transaction,
       where: {
@@ -15,9 +15,20 @@ const checkDebts = async (schemeId, frn, reference, netValue, transaction) => {
         frn: parsedFrn,
         netValue,
         paymentRequestId: null,
-        reference: {
-          [db.Sequelize.Op.like]: `%${referenceNumeric}`
-        }
+        [db.Sequelize.Op.or]: [
+          { reference },
+          { reference: secondaryReference },
+          {
+            reference: {
+              [db.Sequelize.Op.like]: `%${Number(referenceNumeric).toString()}%`
+            }
+          },
+          {
+            reference: {
+              [db.Sequelize.Op.like]: `%${Number(secondaryReferenceNumeric).toString()}%`
+            }
+          }
+        ]
       }
     })
   }
--- test/integration/local/debt/check-debts.test.js
+++ test/integration/local/debt/check-debts.test.js
@@ -1,36 +1,79 @@
 const checkDebts = require('../../../../app/debt/check-debts')
 const db = require('../../../../app/data')
 
-db.debtData.findOne = jest.fn()
-
-const mockTransaction = {}
-
+let mockDebtData
+let expectedDebtData
 describe('checkDebts function', () => {
-  afterEach(() => {
-    jest.clearAllMocks()
+  beforeEach(async () => {
+    await db.debtData.truncate({ cascade: true })
+    await db.scheme.truncate({ cascade: true })
+    mockDebtData = {
+      debtDataId: 1,
+      paymentRequestId: null,
+      schemeId: 1,
+      frn: '1234567890',
+      reference: '123',
+      netValue: '100',
+      debtType: 'irr',
+      recoveryDate: '20/01/2023',
+      attachedDate: null,
+      createdDate: '2018-02-22T11:44:06.157Z',
+      createdBy: 'Developer',
+      createdById: 'cx24291'
+    }
+    expectedDebtData = {
+      debtDataId: 1,
+      paymentRequestId: null,
+      schemeId: 1,
+      frn: '1234567890',
+      reference: '123',
+      netValue: '100',
+      netValueText: '£1.00',
+      debtType: 'irr',
+      debtTypeText: 'Irregular',
+      recoveryDate: '20/01/2023',
+      attachedDate: null,
+      createdDate: new Date('2018-02-22T11:44:06.157Z'),
+      createdBy: 'Developer',
+      createdById: 'cx24291'
+    }
+    await db.scheme.create({
+      schemeId: 1,
+      name: 'SFI'
+    })
   })
 
   test('should return an empty object if frn is not a number', async () => {
-    const result = await checkDebts(1, 'abc', '123A', 100, mockTransaction)
+    const result = await checkDebts(1, 'abc', '123', '456', 100)
     expect(result).toEqual({})
   })
 
+  test('should return matched data if exact match of reference', async () => {
+    await db.debtData.create(mockDebtData)
+    const result = await checkDebts(1, '1234567890', '123', '456', 100)
+    expect(result.get()).toEqual(expectedDebtData)
+  })
+
+  test('should return matched data if exact match of secondaryReference', async () => {
+    await db.debtData.create(mockDebtData)
+    const result = await checkDebts(1, '1234567890', '456', '123', 100)
+    expect(result.get()).toEqual(expectedDebtData)
+  })
+
   test('should return matched data if numeric part of reference matches', async () => {
-    const mockMatchedData = { schemeId: 1, frn: 123, reference: '456A', netValue: 100 }
-    db.debtData.findOne.mockResolvedValueOnce(mockMatchedData)
-    const result = await checkDebts(1, '123', 'A0123', 100, mockTransaction)
-    expect(result).toEqual(mockMatchedData)
-    expect(db.debtData.findOne).toHaveBeenCalledWith({
-      transaction: mockTransaction,
-      where: {
-        schemeId: 1,
-        frn: 123,
-        netValue: 100,
-        paymentRequestId: null,
-        reference: {
-          [db.Sequelize.Op.like]: '%0123'
-        }
-      }
-    })
+    await db.debtData.create(mockDebtData)
+    const result = await checkDebts(1, '1234567890', 'A0123A', 'B0456B', 100)
+    expect(result.get()).toEqual(expectedDebtData)
+  })
+
+  test('should return matched data if numeric part of secondary reference matches', async () => {
+    await db.debtData.create(mockDebtData)
+    const result = await checkDebts(1, '1234567890', 'B0456B', 'A0123A', 100)
+    expect(result.get()).toEqual(expectedDebtData)
+  })
+
+  test('should return null if no match', async () => {
+    const result = await checkDebts(1, '1234567890', 'A0123A', '456B', 100)
+    expect(result).toEqual(null)
   })
 })
