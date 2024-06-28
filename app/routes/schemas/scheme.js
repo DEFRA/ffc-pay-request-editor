fb0e111ada3469d36201837d77d1037e44f6c5b4
Sam Plackett <60177449+samplackett@users.noreply.github.com>
2024-06-28 13:28:42 +0100

    add sfi expanded (#112)

--- app/routes/schemas/scheme.js
+++ app/routes/schemas/scheme.js
@@ -1,9 +1,9 @@
 const Joi = require('joi')
 
 module.exports = {
-  scheme: Joi.string().valid('SFI22', 'SFI Pilot', 'CS', 'BPS', 'FDMR', 'SFI23', 'Vet Visits', 'Lump Sums', 'Delinked').required()
+  scheme: Joi.string().valid('SFI22', 'SFI Pilot', 'CS', 'BPS', 'FDMR', 'SFI23', 'Vet Visits', 'Lump Sums', 'Delinked', 'Expanded SFI Offer').required()
     .messages({
-      'any.only': 'The scheme must be one of the following: SFI22, SFI Pilot, Lump Sums, Vet Visits, CS, BPS, FDMR, SFI23, Delinked.',
+      'any.only': 'The scheme must be one of the following: SFI22, SFI Pilot, Lump Sums, Vet Visits, CS, BPS, FDMR, SFI23, Delinked, Expanded SFI Offer.',
       'any.required': 'A scheme must be selected.'
     })
 }
