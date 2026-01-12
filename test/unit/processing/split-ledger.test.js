39304d6f88226c2b489825f0bf2a728613ff8f08
Oliver Lewington <oliver.lewington@atos.net>
2026-01-12 16:11:44 +0000

    Removing any references of FDMR from code and database (#159)

--- test/unit/processing/split-ledger.test.js
+++ test/unit/processing/split-ledger.test.js
@@ -1,4 +1,4 @@
-const { SFI, SFI_PILOT, LUMP_SUMS, VET_VISITS, CS, BPS, FDMR, SFI23, DELINKED, SFI_EXPANDED, COHTR, COHTC } = require('../../../app/constants/schemes')
+const { SFI, SFI_PILOT, LUMP_SUMS, VET_VISITS, CS, BPS, SFI23, DELINKED, SFI_EXPANDED, COHTR, COHTC } = require('../../../app/constants/schemes')
 const splitToLedger = require('../../../app/processing/ledger/split-to-ledger')
 const { AP, AR } = require('../../../app/processing/ledger/ledgers')
 
@@ -26,7 +26,6 @@ describe('split ledger test', () => {
     { schemeId: VET_VISITS, invoiceNumber: 'AHWR1234567890V002', invoicePrefix: 'AHWR1234567890' },
     { schemeId: CS, invoiceNumber: 'S12345678A123456V002', invoicePrefix: 'S1234567' },
     { schemeId: BPS, invoiceNumber: 'S12345678C123456V002', invoicePrefix: 'S1234567' },
-    { schemeId: FDMR, invoiceNumber: 'F12345678C123456V002', invoicePrefix: 'F1234567' },
     { schemeId: SFI23, invoiceNumber: 'S12345678SFI123456V002', invoicePrefix: 'S1234567' },
     { schemeId: DELINKED, invoiceNumber: 'D12345678SFI123456V002', invoicePrefix: 'D1234567' },
     { schemeId: SFI_EXPANDED, invoiceNumber: 'E12345678E123456V002', invoicePrefix: 'E1234567' },
