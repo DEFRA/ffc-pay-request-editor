fc858c7171b3b3e0f52670d5ed9bf1bbd6dd61cd
Sam Plackett <samuel.plackett@eviden.com>
2026-05-07 17:14:38 +0100

    add fully functional sitemap

--- app/routes/sitemap.js
+++ app/routes/sitemap.js
@@ -1,25 +1,53 @@
-const { SitemapStream, streamToPromise } = require('sitemap')
-const config = require('../config')
+const ViewModel = require('./models/sitemap')
 
 module.exports = {
   method: 'GET',
-  path: '/sitemap.xml',
+  path: '/sitemap',
   options: {
-    auth: false,
-    handler: async (request, h) => {
-      const rawHost = process.env.SITE_HOSTNAME ?? `localhost:${config.port}`
-      const hostname = /^https?:\/\//i.test(rawHost) ? rawHost : `http://${rawHost}`
-      const smStream = new SitemapStream({ hostname })
+    handler: (request, h) => {
+      const sections = [
+        {
+          title: '',
+          links: [
+            { href: '/', text: 'Home' }
+          ]
+        },
+        {
+          title: 'Unattached reporting datasets',
+          links: [
+            { href: '/capture', text: 'Capture reporting data' },
+            { href: '/capture-debt', text: 'Create new reporting dataset' }
+          ]
+        },
+        {
+          title: 'Requests awaiting reporting data',
+          links: [
+            { href: '/enrich', text: 'Requests awaiting reporting data' }
+          ]
+        },
+        {
+          title: 'Awaiting ledger assignment',
+          links: [
+            { href: '/manual-ledger', text: 'Manual Ledger Assignment' }
+          ]
+        },
+        {
+          title: 'Requests awaiting quality check',
+          links: [
+            { href: '/quality-check', text: 'Ledger assignments awaiting quality check' }
+          ]
+        },
+        {
+          title: 'Help',
+          links: [
+            { href: '/accessibility', text: 'Accessibility statement' },
+            { href: '/cookies', text: 'Cookies' },
+            { href: '/privacy', text: 'Privacy' }
+          ]
+        }
+      ]
 
-      const routes = request.server.table()
-        .filter(r => r.method.toUpperCase() === 'GET')
-        .map(r => r.path)
-        .filter(p => !p.includes('{') && !p.startsWith('/static') && p !== '/sitemap.xml')
-
-      routes.forEach(path => smStream.write({ url: path, changefreq: 'weekly', priority: 0.7 }))
-      smStream.end()
-      const xml = (await streamToPromise(smStream)).toString()
-      return h.response(xml).type('application/xml')
+      return h.view('sitemap', new ViewModel(sections))
     }
   }
 }
