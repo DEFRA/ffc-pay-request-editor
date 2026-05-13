988866483ed74edbd2dfa8789cd2d2a192abaf6e
Leigh Godson <leigh.godson@atos.net>
2026-05-13 12:54:32 +0100

    reduce logs

--- app/messaging/process-manual-ledger-data-message.js
+++ app/messaging/process-manual-ledger-data-message.js
@@ -1,10 +1,9 @@
 const { processManualLedgerRequest } = require('../manual-ledger')
-const util = require('util')
 
 const processManualLedgerDataMessage = async (message, receiver) => {
   try {
     const manualLedgerRequest = message.body
-    console.log('Payment request received for manual ledger check', util.inspect(manualLedgerRequest, false, null, true))
+    console.log('Payment request received for manual ledger check', { frn: manualLedgerRequest.frn, sbi: manualLedgerRequest.sbi, invoiceNumber: manualLedgerRequest.invoiceNumber })
     await processManualLedgerRequest(manualLedgerRequest)
     await receiver.completeMessage(message)
   } catch (err) {
