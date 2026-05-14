da9b5b740b82d04828bffc3bcbaa114e71481844
Sam Plackett <samuel.plackett@eviden.com>
2026-05-14 15:12:28 +0100

    standardise approach to sitemap

---
+++ app/constants/sitemap.js
@@ -0,0 +1,33 @@
+const HOME = { href: '/', text: 'Home' }
+
+const CAPTURE_LINKS = [
+  { href: '/capture', text: 'Capture reporting data' },
+  { href: '/capture-debt', text: 'Create new reporting dataset' }
+]
+
+const ENRICH_LINKS = [
+  { href: '/enrich', text: 'Requests awaiting reporting data' }
+]
+
+const MANUAL_LEDGER_LINKS = [
+  { href: '/manual-ledger', text: 'Manual Ledger Assignment' }
+]
+
+const QUALITY_CHECK_LINKS = [
+  { href: '/quality-check', text: 'Ledger assignments awaiting quality check' }
+]
+
+const HELP_LINKS = [
+  { href: '/accessibility', text: 'Accessibility statement' },
+  { href: '/cookies', text: 'Cookies' },
+  { href: '/privacy', text: 'Privacy' },
+]
+
+module.exports = [
+  { title: '', links: [HOME] },
+  { title: 'Unattached reporting datasets', links: CAPTURE_LINKS },
+  { title: 'Requests awaiting reporting data', links: ENRICH_LINKS },
+  { title: 'Awaiting ledger assignment', links: MANUAL_LEDGER_LINKS },
+  { title: 'Requests awaiting quality check', links: QUALITY_CHECK_LINKS },
+  { title: 'Help', links: HELP_LINKS },
+]
